fa7cc069ce58669ad0e9befa58acb35ebe2edefb
Brendan Weibrecht <brendan@weibrecht.net.au>
2015-09-28 14:43:05 +1000

    Turn each script into a module

--- mirror-channel.rb
+++ mirror-channel.rb
@@ -2,49 +2,55 @@
 
 require 'unirest'
 
+require_relative 'twitch-downloader.rb'
+
 require 'shellwords'
 
-def api_video_list_for_channel channel
-  api_url = "https://api.twitch.tv/kraken/channels/#{channel}/videos?broadcasts=true&limit=100"
-  headers = {
-    "Accept" => "application/vnd.twitchtv.v3+json",
-    "Client-ID" => IO.read('.twitch-developer-client-id').chomp,
-  }
-  puts "Querying API..."
-  response = Unirest.get api_url, headers: headers
-  response.body["videos"]
-end
+module MirrorTwitchChannel
 
-def download_videos videos
-  videos.each do |v|
-    date = v["recorded_at"].gsub('Z','').gsub('T','_').gsub(':','-')
-    url = v["url"]
-    title = v["title"]
-    id = url.split('/').last
-    name = "%s - %s - %s" % [date, id, title]
-    download_video_to_folder url, name
+  def api_video_list_for_channel channel
+    api_url = "https://api.twitch.tv/kraken/channels/#{channel}/videos?broadcasts=true&limit=100"
+    headers = {
+      "Accept" => "application/vnd.twitchtv.v3+json",
+      "Client-ID" => IO.read('.twitch-developer-client-id').chomp,
+    }
+    puts "Querying API..."
+    response = Unirest.get api_url, headers: headers
+    response.body["videos"]
   end
-end
 
-def download_video_to_folder url, name
-  if Dir.exist? name
-    puts "SKIPPED - directory already exists: #{name}"
-  else
-    puts "Downloading: #{name}"
-    Dir.mkdir name
-    name_escaped = Shellwords.escape name
-    Dir.chdir name do
-      download_video
+  def download_videos videos
+    videos.each do |v|
+      date = v["recorded_at"].gsub('Z','').gsub('T','_').gsub(':','-')
+      url = v["url"]
+      title = v["title"]
+      id = url.split('/').last
+      name = "%s - %s - %s" % [date, id, title]
+      download_video_to_folder url, name
     end
   end
-end
 
-def download_video
-  
+  def download_video_to_folder url, name
+    if Dir.exist? name
+      puts "SKIPPED - directory already exists: #{name}"
+    else
+      puts "Downloading: #{name}"
+      Dir.mkdir name
+      name_escaped = Shellwords.escape name
+      Dir.chdir name do
+        download_video
+      end
+    end
+  end
+
+  def download_video
+
+  end
+
 end
 
 if __FILE__ == $0
   videos = api_video_list_for_channel ARGV[0]
   # require 'pry'; binding.pry
-  download_videos videos
+  MirrorTwitchChannel.download_videos videos
 end
--- twitch-downloader.rb
+++ twitch-downloader.rb
@@ -7,45 +7,49 @@ require_relative 'ruby-progressbar-twoline/ruby-progressbar-twoline.rb'
 require 'json'
 require 'uri'
 
-def download_video_by_url url
-  puts "Downloading #{url}"
-  vod_id = url.split("/")[-1]
+module TwitchDownloader
 
-  token_url = "https://api.twitch.tv/api/vods/#{vod_id}/access_token?as3=t"
-  token = JSON.parse(fetch(token_url, "token"))
+  def download_video_by_url url
+    puts "Downloading #{url}"
+    vod_id = url.split("/")[-1]
 
-  qualities_list_url = "http://usher.justin.tv/vod/#{vod_id}?nauthsig=#{token["sig"]}&nauth=#{token["token"]}"
-  qualities_list = fetch(qualities_list_url, "qualities list")
+    token_url = "https://api.twitch.tv/api/vods/#{vod_id}/access_token?as3=t"
+    token = JSON.parse(fetch(token_url, "token"))
 
-  chunk_list_url = qualities_list.split("\n")[3].gsub(%r{/(high|medium|low|mobile)/},'/chunked/')
-  chunk_list = fetch(chunk_list_url, "chunk list").split("\n").select { |c| c[0] != '#' && c != '' }
+    qualities_list_url = "http://usher.justin.tv/vod/#{vod_id}?nauthsig=#{token["sig"]}&nauth=#{token["token"]}"
+    qualities_list = fetch(qualities_list_url, "qualities list")
 
-  dl_url = "http://#{chunk_list_url.split("/")[2..-2].join("/")}"
-  download_video_chunks dl_url, chunk_list, vod_id
-end
+    chunk_list_url = qualities_list.split("\n")[3].gsub(%r{/(high|medium|low|mobile)/},'/chunked/')
+    chunk_list = fetch(chunk_list_url, "chunk list").split("\n").select { |c| c[0] != '#' && c != '' }
 
-def fetch uri, name = nil
-  puts "Fetching #{name} from: #{uri}" if name
-  RestClient.get(URI.escape(uri))
-end
+    dl_url = "http://#{chunk_list_url.split("/")[2..-2].join("/")}"
+    download_video_chunks dl_url, chunk_list, vod_id
+  end
 
-def download_video_chunks dl_url, chunk_list, filename
-  open("#{filename}.ts", "wb") do |file|
-    list_size = chunk_list.size
-    progressbar = ProgressBar.create(
-      format: "%t %b%i\n%a %E Processed: %c of %C, %P%",
-      total: list_size,
-      remainder_mark: '･',
-    )
-    puts "Downloading #{list_size} video parts..."
-    chunk_list.each_with_index do |part, i|
-      url = "#{dl_url}/#{part}"
-      progressbar.log(url)
-      response = fetch(url)
-      file.write(response.body)
-      progressbar.increment
+  def fetch uri, name = nil
+    puts "Fetching #{name} from: #{uri}" if name
+    RestClient.get(URI.escape(uri))
+  end
+
+  def download_video_chunks dl_url, chunk_list, filename
+    open("#{filename}.ts", "wb") do |file|
+      list_size = chunk_list.size
+      progressbar = ProgressBar.create(
+        format: "%t %b%i\n%a %E Processed: %c of %C, %P%",
+        total: list_size,
+        remainder_mark: '･',
+      )
+      puts "Downloading #{list_size} video parts..."
+      chunk_list.each_with_index do |part, i|
+        url = "#{dl_url}/#{part}"
+        progressbar.log(url)
+        response = fetch(url)
+        file.write(response.body)
+        progressbar.increment
+      end
     end
   end
+
 end
 
 if __FILE__ == $0
@@ -54,5 +58,5 @@ if __FILE__ == $0
     exit
   end
   url = ARGV[0]
-  download_video_by_url url
+  TwitchDownloader.download_video_by_url url
 end
